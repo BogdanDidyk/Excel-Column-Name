57d930b6118d9e6faf5c4aea89d9ec1d97ca756e
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-22 23:05:08 +0300

    Added the function that returns Excel column number by name

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function isAllUpperLatinCharacters(str) {
     return /^[A-Z]+$/.test(str);
 }
+
+function getExcelColumnNumberByName(columnName) {
+    if (typeof columnName != "string" || !isAllUpperLatinCharacters(columnName)) return NaN;
+
+    const len = columnName.length;
+    let pow = 0;
+    let result = 0;
+
+    for (let index = len - 1; index >= 0; index--) {
+        let code = columnName.charCodeAt(index) - 65 + 1;
+        result += code * Math.pow(26, pow);
+        pow++;
+    }
+
+    return result;
+}
